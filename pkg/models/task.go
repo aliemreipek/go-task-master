09ae419e29abed30734ecce84b58da7a7dbe0201
aliemreipek <aliemre.ipek@outlook.com>
2026-02-01 16:44:29 +0300

    Fixed a comment in models/task.go .

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -14,5 +14,5 @@ type Task struct {
 	DueDate     time.Time      `json:"due_date"`
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"` // Soft delete özelliği
+	DeletedAt   gorm.DeletedAt `gorm:"index" json:"-"` // Soft delete
 }
